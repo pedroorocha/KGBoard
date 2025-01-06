b986ee895704b1cd288a2ec31369c27c3a9ef150
Italo Vicente <114747617+ItaloVicente@users.noreply.github.com>
2025-01-06 17:04:49 -0300

    Botoes do Menu

--- scripts/menu_principal.gd
+++ scripts/menu_principal.gd
@@ -9,3 +9,24 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_jogadores_pressed() -> void:
+	$PopupJogadores.popup_centered()
+	pass # Replace with function body.
+
+
+func _on_regras_pressed() -> void:
+	$PopupRegras.popup_centered()
+	pass # Replace with function body.
+
+
+func _on_voltar_pressed() -> void:
+	$PopupRegras.hide()
+	pass # Replace with function body.
+
+
+func _on_iniciar_jogo_pressed() -> void:
+	var jogadores = $PopupJogadores/HBoxContainer/OptionButton.get_selected_id() + 2
+	get_tree().change_scene("res://scenes/tabuleiro.tscn")
+	pass # Replace with function body.
